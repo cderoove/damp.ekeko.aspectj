bb115804dea4f575557553a43f1d0958ee5f1162
Coen De Roover <cderoove@vub.ac.be>
2012-09-12 16:52:34 -0300

    Added some example queries.

--- EkekoAspectJ/src/damp/ekeko/aspectj.clj
+++ EkekoAspectJ/src/damp/ekeko/aspectj.clj
@@ -2,7 +2,7 @@
   damp.ekeko.aspectj
   (:refer-clojure :exclude [== type])
 ;  (:use [clojure.core.logic])
-;  (:use [damp.ekeko logic])
+  (:use [damp.ekeko logic])
   (:use [damp.ekeko])
   (:require [damp.ekeko.aspectj [reification :as aj] [ajdtastnode :as ajnode]]))
 
@@ -20,9 +20,22 @@
   (use 'damp.ekeko.aspectj)
   (in-ns 'damp.ekeko.aspectj)
   
-  (ekeko* [?aspect ?pc] (aj/aspect-advice ?aspect ?pc))
+  ;results from model elements
+  (ekeko* [?aspect ?advice] (aj/aspect-advice ?aspect ?advice))
   
+  (ekeko* [?aspect ?pc] (aj/aspect-pointcut ?aspect ?pc))
+  
+  (ekeko* [?key ?element] (aj/element ?key ?element))
+  
+  ;results from xcut model
+  (ekeko* [?advice ?shadow] (aj/advice-shadow ?advice ?shadow))
 
+  ;;all xcut relations and their type ... only 4 contain info for the test project
+  ;;TODO: check on HealthWatcher, AJHotdraw
+  (ekeko* [?xcut ?reltype ?rel] (aj/xcut-relationtype-relation ?xcut ?reltype ?rel))
+  
+  
+  
   )
 
 
